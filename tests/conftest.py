d7e724bad727061d82bb33cbb039a88383bff161
Анна Погребник <apogrebnik30@gmail.com>
2024-04-28 21:17:37 +0300

    add tests for petstore

---
+++ tests/conftest.py
@@ -0,0 +1,24 @@
+import logging
+
+import pytest
+
+
+def pytest_addoption(parser):
+    parser.addoption("--url", action='store', default='https://petstore.swagger.io/v2/')
+    parser.addoption("--log_level", action="store", default="INFO")
+
+
+@pytest.fixture(scope='session', autouse=True)
+def url(request):
+    return request.config.getoption("--url")
+
+
+@pytest.fixture(scope='session', autouse=True)
+def logger_test(request):
+    logger = logging.getLogger('testing')
+    log_level = request.config.getoption("--log_level")
+    file_handler = logging.FileHandler(f"logs/{request.node.name}.log")
+    file_handler.setFormatter(logging.Formatter('%(levelname)s %(message)s'))
+    logger.addHandler(file_handler)
+    logger.setLevel(level=log_level)
+    return logger
